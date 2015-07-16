e9a1318012bb946454a2703c71a4bcf809de45c8
Danny Evenwel <danny@driebit.nl>
2015-07-16 11:04:39 +0200

    player screen

--- web/js/player/player.js
+++ web/js/player/player.js
@@ -1,25 +1,38 @@
 var player = (function() {
 
     var me = this,
+        currentScreen;
+
 
     function init () {
+
         console.log('init player app, start init other modules');
 
         imageCapture.init();
+        ui.init();
 
         //handlers
-        $('#capture').on('click', handleCapture);
+        //$('#capture').on('click', handleCapture);
 
         imageCapture.showStream();
 
-        console.log(test);
-    
     }
 
 
     function handleCapture() {
         var image = imageCapture.captureImage();
     }
+
+    function handleTap() {
+
+
+    }
+
+    
+
+
+
+
 
     return {
         init: init
---
+++ web/js/player/ui.js
@@ -0,0 +1,35 @@
+var ui = (function() {
+
+    function init() {
+
+        console.log('init ui module');
+
+        $('#').on('click', handleGroupSelect);
+        //$('#').on('click', );
+
+    }
+
+
+    function handleGroupSelect() {
+
+
+    }
+
+
+    function goToScreen(screen) {
+
+        //return screen name
+
+    }
+
+
+    return {
+
+        init: init,
+        goToScreen: goToScreen
+
+    }
+  
+})();
+
+    
