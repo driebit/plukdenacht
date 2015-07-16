ac6bd47d91088223183cd97c268176f5d8899934
Dorien <dorien@driebit.nl>
2015-07-16 10:35:53 +0200

    css en gulp

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
+
+    CSS_SOURCE   = 'web/css/src',
+    CSS_BUILD    = 'web/css/build',
+    //JS_SOURCE    = 'web/js/src',
+    //JS_BUILD     = 'web/js/build';
+    TEMPLATES    = 'web'
+
+gulp.task('default', ['styles', 'watch']);
+
+gulp.task('watch', function () {
+    gulp.watch(
+        [CSS_SOURCE + '/**/*.scss'], ['styles']);
+});
+
+gulp.task('styles', function () {
+    gulp.src(CSS_SOURCE + '/screen.scss')
+        .pipe(sass({errLogToConsole: true}))
+        .pipe(gulp.dest(CSS_BUILD));
+});
